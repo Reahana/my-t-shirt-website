ab1ea270945cf8d931f84cf8b74a78494d66cbe8
Reahana <reahana233@gmail.com>
2024-02-14 21:01:24 +0600

    navbar

--- src/componants/Header/Header.jsx
+++ src/componants/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Nav from '../Nav/Nav';
 
 const Header = () => {
     return (
         <div>
-            <h1>this is header</h1>
+            <Nav></Nav>
         </div>
     );
 };
--- src/componants/Nav/Nav.jsx
+++ src/componants/Nav/Nav.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <div>
-            <h1>nav</h1>
-        </div>
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/review'>Order Review</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+        </nav>
     );
 };
 
