03f2f4fea9818c2b300c364c9d9f687a184e8b16
Reahana <reahana233@gmail.com>
2024-02-14 23:49:15 +0600

    home

--- src/componants/Home/Home.jsx
+++ src/componants/Home/Home.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
     const tshirts = useLoaderData();
     return (
-        <div>
-            <h2>tshirt : {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='tshirts-container'>
+            {
+                tshirts.map(tshirt => <Tshirt
+                key={tshirt._id}
+                tshirt={tshirt}
+                ></Tshirt>)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
