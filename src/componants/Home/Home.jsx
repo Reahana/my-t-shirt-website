1bbd0b17acc68daa5aec301b09d89c677a4a3bb5
Reahana <reahana233@gmail.com>
2024-02-16 12:32:57 +0600

    handle add remove toast

--- src/componants/Home/Home.jsx
+++ src/componants/Home/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../Tshirt/Tshirt';
 import Cart from '../Cart/Cart';
 import './Home.css'
+import toast from 'react-hot-toast';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt =>{
+        const exists = cart.find(ts =>ts._id === tshirt._id)
+        if(exists){
+toast('You have already added this t-shirt');
+        }
+        else{
+            const newCart = [...cart,tshirt]
+            setCart(newCart);
+        }
+        
+    }
+    const handleRemoveFromCart =id =>{
+       const remaining = cart.filter(ts => ts._id !== id);
+       setCart(remaining)
+    }
     return (
         <div className='home-container'>
             <div className='tshirts-container'>
@@ -13,11 +31,14 @@ const Home = () => {
                 tshirts.map(tshirt => <Tshirt
                 key={tshirt._id}
                 tshirt={tshirt}
+                handleAddToCart ={handleAddToCart}
                 ></Tshirt>)
             }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart 
+                cart={cart}
+                handleRemoveFromCart ={handleRemoveFromCart}></Cart>
             </div>
         </div>
     );
--- src/componants/Tshirt/Tshirt.jsx
+++ src/componants/Tshirt/Tshirt.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Tshirt.css'
 
-const Tshirt = ({tshirt}) => {
+const Tshirt = ({tshirt,handleAddToCart}) => {
     const {picture, name , price} = tshirt;
     return (
         <div className='t-shirt'>
             <img src={picture} alt="" />
             <h3>{name}</h3>
             <p>Price: $ {price}</p>
-            <button>Buy Now</button>
+            <button onClick={()=> handleAddToCart(tshirt)}>Buy Now</button>
         </div>
     );
 };
