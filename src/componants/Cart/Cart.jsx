88c4caa27fbff71890aedea21b9fa7f232d390fa
Reahana <reahana233@gmail.com>
2024-04-14 11:44:53 +0600

    conditional css

--- src/componants/Cart/Cart.jsx
+++ src/componants/Cart/Cart.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = ({cart,handleRemoveFromCart}) => {
-
+let messeage;
+if (cart.length === 0){
+    messeage =<p>Please add in the cart</p>
+}
+else{
+    messeage = <h3>Thank you</h3>
+}
     return (
         <div>
-            <h2>Order Summary {cart.length}</h2>
+            <h2 className={cart.length === 1 ? 'blue' : 'red'}>Order Summary {cart.length}</h2>
+           <p className={`bold border ${cart.length === 4 ? 'orange':'green'}`}>Something</p>
+            {cart.length > 2 ? <span className='yellow'>more buy</span> : <span>ok</span>}
+            {messeage}
             {
                 cart.map(tshirt => 
                     <p 
@@ -13,6 +22,12 @@ const Cart = ({cart,handleRemoveFromCart}) => {
                         <button onClick={()=> handleRemoveFromCart(tshirt._id)}>X</button>
                     </p>
                     )
+            }
+            {
+                cart.length === 2 && <p>Double Delight</p>
+            }
+            {
+                cart.length === 3 || <h3>3 hote hobe</h3>
             }
         </div>
     );
