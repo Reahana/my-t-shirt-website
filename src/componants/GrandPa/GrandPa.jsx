b08a9111a276b9d2c36cca344956773befe39f2a
Reahana <reahana233@gmail.com>
2024-04-14 12:20:24 +0600

    grandpa

--- src/componants/GrandPa/GrandPa.jsx
+++ src/componants/GrandPa/GrandPa.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Father from '../Father/Father';
+import Uncle from '../Uncle/Uncle';
+import Aunty from '../Aunty/Aunty';
+import './GrandPa.css'
 
 const GrandPa = () => {
     return (
-        <div>
+        <div className='grandpa'>
             <h2>Grandpa</h2>
+            <section className='flex'>
+                <Father></Father>
+                <Uncle></Uncle>
+                <Aunty></Aunty>
+            </section>
         </div>
     );
 };
