cad1f343d9849150d27e8a0330c69053a4d04a7e
Reahana <reahana233@gmail.com>
2024-04-14 19:59:37 +0600

    api drilling

--- src/componants/GrandPa/GrandPa.jsx
+++ src/componants/GrandPa/GrandPa.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 import './GrandPa.css'
+export  const RingContext = createContext('gold')
+export const MoneyContext = createContext(0);
 
 const GrandPa = () => {
     const ring ='diamond'
+   const [money, setMoney] = useState(0)
     return (
         <div className='grandpa'>
             <h2>Grandpa</h2>
-            <section className='flex'>
-                <Father ring ={ring}></Father>
-                <Uncle></Uncle>
-                <Aunty ring ={ring}></Aunty>
-            </section>
+            <p>Has money :{money}</p>
+          <MoneyContext.Provider value={[money,setMoney]}>
+                <RingContext.Provider value='golden ring'>
+                        <section className='flex'>
+                                <Father ring ={ring}></Father>
+                                <Uncle></Uncle>
+                                <Aunty ring ={ring}></Aunty>
+                            </section>  
+                </RingContext.Provider>
+          </MoneyContext.Provider>
         </div>
     );
 };
--- src/componants/Special/Special.jsx
+++ src/componants/Special/Special.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../GrandPa/GrandPa';
 
 const Special = ({ring}) => {
+    const myRing = useContext(RingContext)
     return (
         <div>
             <h2>Special</h2>
-            <p>Ring: {ring}</p>
+            <p>Ring: {myRing}</p>
         </div>
     );
 };
